ede706e4d3cdbf42bea355533c06631cfc87bbb3
wontae0924 <wontae0924@gmail.com>
2024-02-14 20:10:05 +0900

    [Feat] Header , Footer 개발 및 tailwind 설치(#1)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<AppHeader />
+    <router-view/>
+	<AppFooter/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import AppHeader  from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 요기요 컬러 #ff7799 하지만 tailwind는 따로 세팅 해줘야 합니다. 최대한 비슷한 색깔로 했습니다. -->
+  <div class="bg-rose-600">
+  <div class="w-full h-[84px] mx-auto max-w-[1400px] flex items-center justify-between px-4">
+      <div class="flex items-center space-x-10">
+          <div class="grid-cols-4 col-span-4">
+              <h1 class="text-slate-50 font-serif text-2xl py-4 ml-2">포장의민족</h1>
+          </div>
+      </div>
+      <div class="flex items-center space-x-4">
+          <button class="rounded-md text-slate-50 border-2 border-slate-50 p-2 px-8">로그인</button>
+          <button class="rounded-md text-slate-50 bg-orange-500 p-2 px-8">주문표(0)</button>
+      </div>
+  </div>
+</div>
+
+
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
