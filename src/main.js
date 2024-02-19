be9bf0e13de9985b9cdf88d283afeb9eb7baec38
wontae0924 <wontae0924@gmail.com>
2024-02-19 14:26:00 +0900

    [Feat] axios, vue 환경세팅 설정 (#6)

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import router from '@/router/index.js'
+import axios from 'axios'
 
 const app = createApp(App);
-
+// 401응답의 경우 inteceptor를 통해 공통적으로 토큰 제거 후 로그아웃처리
+axios.interceptors.response.use(response => response, error =>{
+  if(error.response && error.response.status === 401 || error.response.status === 403){
+    localStorage.clear();
+    window.location.href = "/login";
+  }
+  return Promise.reject(error);
+})
 app.use(router);
 app.mount('#app');
