f1aaffd51f6be94c19d8afefc829b7c88c97d56b
wontae0924 <wontae0924@gmail.com>
2024-02-20 21:19:13 +0900

    Feat/Store 매장조회개발(#8)

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import {createRouter,createWebHistory} from 'vue-router';
 import HomeComponent from '@/components/HomeComponent.vue';
+import StoreCategories from '@/views/StoreCategories.vue';
 const routes = [
   {path:'/' , name: 'HOME' , component: HomeComponent},
+  {path:'/categories' , name: 'StoreCategories' , component: StoreCategories ,props: true},
+  
 ]
 const router = createRouter({
   history: createWebHistory(),
