0eae17058cae3c6f6efb6b163b5e526dfb439eed
BellWin98 <jongseunghan@naver.com>
2024-02-20 18:55:04 +0900

    [Feat] 마이페이지 API 구현(정보 수정 제외)(#)

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,21 @@
 import {createRouter,createWebHistory} from 'vue-router';
 import HomeComponent from '@/components/HomeComponent.vue';
 import LoginComponent from '@/components/Login_User.vue';
-import MypageComponent from '@/components/MyPage.vue';
-import UpdateMyInfoComponent from '@/components/UpdateMyInfo.vue';
-import MyOrderListComponent from '@/components/orderList.vue';
+
+// export default인 경우에는 {} 필요없고, 여러개 요소가 있을 경우 {} 필요
+import { memberRoutes } from "./memberRouter.js";
+import { orderRoutes } from "./orderRouter.js";
+import { menuRoutes } from "./menuRouter.js";
+import { storeRoutes } from "./storeRouter.js";
+
 const routes = [
   {path:'/' , name: 'HOME' , component: HomeComponent},
-  
   {path:'/login' , name: 'Login' , component: LoginComponent},
-  {path:'/mypage' , name: 'MyPage' , component: MypageComponent},
-  {path:'/update/myinfo' , name: 'UpdateMyInfo' , component: UpdateMyInfoComponent},
-  {path:'/myOrderList' , name: 'MyOrderList' , component: MyOrderListComponent},
+
+  ...memberRoutes,
+  ...orderRoutes,
+  ...menuRoutes,
+  ...storeRoutes,
 ]
 const router = createRouter({
   history: createWebHistory(),
