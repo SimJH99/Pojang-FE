bba95d611bcd9d9fb2472c746f812e2b87640657
SimJH99 <s99s4481@gmail.com>
2024-02-21 18:17:06 +0900

    [Feat] 매장 리스트 페이지구현, 매장 상세정보 페이지 구현(#31)

--- src/views/Store/MyStores.vue
+++ src/views/Store/MyStores.vue
@@ -5,7 +5,7 @@
           <li class="text-lg font-bold mb-2"><a href="/my-info-owner">내 정보</a></li>
           <li class="text-lg font-bold mb-2"><a href="/my-info-update">내 정보 수정</a></li>
           <li class="text-lg font-bold mb-2"><a href="/store-create">매장 등록</a></li>
-          <li class="text-lg font-bold mb-2"><a href="/my-stores">내 가게</a></li>
+          <li class="text-lg font-bold mb-2"><a href="/my-stores">내 매장리스트</a></li>
           <!-- 추가적인 카테고리 항목들을 필요에 따라 나열하세요 -->
       </ul>
   </div>
@@ -13,7 +13,8 @@
     <div class="w-5/6 p-8 flex flex-col">
       <h2 class="text-3xl font-semibold mb-6 text-gray-800">내가게</h2>
       <div v-if="stores.length > 0" class="grid grid-cols-1 gap-4">
-        <div v-for="store in stores" :key="store.id" class="bg-white rounded-md shadow-md p-6">
+
+        <router-link v-for="store in stores" :key="store.id" class="bg-white rounded-md shadow-md p-6" :to="MyStoreInfoId(store.id)">
           <h3 class="text-xl font-semibold text-gray-800 mb-2">매장 등록 번호: {{ store.id }}</h3>
           <a href="/my-store-info">
             <div class="bg-white rounded-md shadow-md p-6" flex>
@@ -21,7 +22,7 @@
               <p class="text-gray-600"><img :src="getImage(store.id)" class="h-24 w-auto mt-2"></p>
             </div>
           </a>
-        </div>
+        </router-link>
       </div>
       <div v-else class="bg-white p-4 rounded-md">
         <p>주문 내역이 없습니다.</p>
@@ -32,7 +33,7 @@
 
 <script>
 import axios from 'axios';
-
+import MyStoreInfo from '@/views/Store/MyStoreInfo';
 export default {
   data() {
     return {
@@ -43,6 +44,9 @@ export default {
     this.fetchOrders();
   },
   methods: {
+    components:{
+      MyStoreInfo
+    },  
     async fetchOrders() {
       try {
         const token = localStorage.getItem('token');
@@ -57,6 +61,9 @@ export default {
     getImage(id) {
       return `${process.env.VUE_APP_API_BASE_URL}/api/stores/${id}/image`;
     },
+    MyStoreInfoId(StoreId){
+      return { path: `/${StoreId}/my-store-info`, params: {id: StoreId}}
+    }
   },
 };
 </script>
