02bca4ca6f42fbd0b31b04cf48e196ae065c5435
BellWin98 <jongseunghan@naver.com>
2024-02-21 21:35:06 +0900

    [Feat] 장바구니 전역 상태 관리를 위한 store 적용(#42)

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './style.css'
 import router from '@/router/index.js'
 import axios from 'axios'
+import store from './store/cart.js'
 
 const app = createApp(App);
 // 401응답의 경우 inteceptor를 통해 공통적으로 토큰 제거 후 로그아웃처리
@@ -14,4 +15,5 @@ axios.interceptors.response.use(response => response, error =>{
   return Promise.reject(error);
 })
 app.use(router);
+app.use(store);
 app.mount('#app');
