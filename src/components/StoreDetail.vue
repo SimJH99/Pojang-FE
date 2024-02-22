803971889d80a296b00880a067e02a3d6f6e115b
BellWin98 <jongseunghan@naver.com>
2024-02-22 11:47:01 +0900

    [Feat] storeId 하위 컴포넌트로 달

--- src/components/StoreDetail.vue
+++ src/components/StoreDetail.vue
@@ -15,7 +15,6 @@
         >♥</button>
           <span class="font-bold text-red-500">{{ store.likes }}</span>
         </div>
-        <div>프롭스 :  {{ $route.params.id }}</div>
       </div>
     </div>
     
@@ -33,7 +32,9 @@
 
     <div v-if="tab === '메뉴'" class="bg-white p-4 shadow-md">
       <ul class="mt-4">
-        <MenuListComponent/>
+        <MenuListComponent
+        :storeId="store.id"
+        />
       </ul>
     </div>
 
@@ -62,11 +63,7 @@ import axios from 'axios';
 import ReviewListComponent from '@/components/ReviewList.vue';
 import MenuListComponent from '@/components/MenuList.vue';
 export default {
-  props: {
-            id: {
-                type: String,
-            }
-          },
+  props:['id'],
   components:{ 
         ReviewListComponent,
         MenuListComponent
@@ -79,14 +76,14 @@ export default {
       isLike: null,
     };
   },
-  created() {
+  mounted() {
     this.fetchStore();
     this.fetchLike();
   },
   methods: {
         async fetchStore() {
             try {
-            const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/stores/${this.$route.params.id}/details`);
+            const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/stores/${this.id}/details`);
             this.store = response.data.result;
             }catch(error) {
                 console.log(error);
@@ -96,14 +93,14 @@ export default {
           try {
             const token = localStorage.getItem('token');
             const headers = { Authorization: `Bearer ${token}` };
-            const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/stores/1/favorite`, { headers });
+            const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/stores/${this.id}/favorite`, { headers });
             this.isLike = response.data;
             }catch(error) {
                 console.log(error);
             } 
         },
-        getImage(id) {
-            return `${process.env.VUE_APP_API_BASE_URL}/api/stores/${id}/image`;
+        getImage(storeId) {
+            return `${process.env.VUE_APP_API_BASE_URL}/api/stores/${storeId}/image`;
         },
         async setLike() {
           const token = localStorage.getItem('token');
