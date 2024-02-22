652961901b22aaf0037926fd7a6ccb1a8ef1ee89
BellWin98 <jongseunghan@naver.com>
2024-02-22 22:13:03 +0900

    수정

--- src/views/Store/StoreOrders.vue
+++ src/views/Store/StoreOrders.vue
@@ -32,11 +32,11 @@
                     </div>
                     <p class="text-gray-800 font-bold mt-2">총 주문 금액: {{ order.totalPrice }}원</p><br />
                     <div>
-                        <button v-if="order.orderStatus == 'PENDING'" @click.prevent="acceptOrder(order.orderId)"
+                        <button v-if="order.orderStatus == 'PENDING'" @click="acceptOrder(order.orderId)"
                             class="bg-blue-500 text-white px-4 py-2 rounded-md mr-2">접수</button>
-                        <button v-if="order.orderStatus == 'PENDING'" @click.prevent="cancelOrder(order.orderId)"
+                        <button v-if="order.orderStatus == 'PENDING'" @click="cancelOrder(order.orderId)"
                             class="bg-red-500 text-white px-4 py-2 rounded-md mr-2">취소</button>
-                        <button v-if="order.orderStatus == 'ORDERED'" @click.prevent="confirmOrder(order.orderId)"
+                        <button v-if="order.orderStatus == 'ORDERED'" @click="confirmOrder(order.orderId)"
                             class="bg-green-500 text-white px-4 py-2 rounded-md">확정</button>
                     </div>
                 </div>
