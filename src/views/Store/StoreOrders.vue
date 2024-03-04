0c225523eacea2f7399637f7911eb98508244610
BellWin98 <jongseunghan@naver.com>
2024-03-04 21:12:19 +0900

    [Fix] 가게 주문 내역 코드 수정
    1. 메뉴, 메뉴 옵션 정보 표시

--- src/views/Store/StoreOrders.vue
+++ src/views/Store/StoreOrders.vue
@@ -6,30 +6,22 @@
             <div v-if="orders.length > 0" class="grid grid-cols-1 gap-4">
                 <div class="bg-white rounded-md shadow-md p-6" v-for="order in orders" :key="order.orderId">
                     <h3 class="text-2xl font-semibold text-gray-800 mb-2">주문 번호: {{ order.orderId }}</h3>
-                    <p class="text-gray-600">주문 일자: {{ order.orderDateTime }}</p>
-                    <p class="text-gray-600">고객명: {{ order.customer }}</p>
-                    <p class="text-gray-600">고객 전화번호: {{ order.phoneNumber }}</p>
-                    <p class="text-gray-600">요청 사항: {{ order.requirement }}</p>
-                    <p class="text-gray-600">주문 상태: {{ order.orderStatus }}</p>
-                    <div class="text-gray-600 mt-5">
-                        <h3 class="text-xl font-semibold text-gray-800 mb-2">메뉴</h3>
+                    <p class="text-gray-600">- 주문 상태: {{ order.orderStatus }}</p>
+                    <p class="text-gray-600">- 주문 일자: {{ order.orderDateTime }}</p>
+                    <p class="text-gray-600">- 고객명: {{ order.customer }}</p>
+                    <p class="text-gray-600">- 고객 전화번호: {{ order.phoneNumber }}</p>
+                    <p class="text-gray-600">- 요청 사항: {{ order.requirement }}</p>
+                    <p class="text-gray-600">
+                        <span class="text-gray-700 font-bold">- 메뉴</span>
                         <ul>
-                            <li v-for="(quantity, menuName) in order.orderMenuInfo" :key="menuName">
-                                {{ menuName }} (수량: {{ quantity }}개)
+                            <li v-for="menu in order.orderMenus" :key="menu.orderMenuId" class="pl-2">
+                            <span class="text-gray-600 font-bold">{{ menu.menuName }}</span> ({{ menu.quantity }}개)
+                            <ul v-for="option in menu.orderMenuOptions" :key="option.orderMenuOptionId" class="pl-4">
+                                <li class="text-sm text-gray-500"> {{ option.optionName }}</li>
+                            </ul>
                             </li>
                         </ul>
-                    </div>
-                    <div class="text-gray-600 mt-5">
-                        <h3 class="text-xl font-semibold text-gray-800 mb-2">메뉴 옵션</h3>
-                        <ul>
-                            <li class="mt-3" v-for="(options, menuName) in order.orderMenuOptions" :key="menuName">
-                                <b>{{ menuName }}</b>
-                                <ul>
-                                    <li class="text-gray-400" v-for="option in options" :key="option">{{ option }}</li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
+                    </p>
                     <p class="text-gray-800 font-bold mt-2">총 주문 금액: {{ order.totalPrice }}원</p><br />
                     <div>
 
@@ -68,7 +60,7 @@ export default {
                 this.orders = response.data.result;
                 console.log(this.orders);
             } catch (error) {
-                console.log(error.response.data.message);
+                console.error(error);
                 alert(error.response.data.message);
             }
         },
